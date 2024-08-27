af40ecda368916af91beb9ab48730cd916a2db21
Prathesh-Yadav <pratheshyadav67@gmail.com>
2024-08-27 14:08:47 +0000

    updated file1.py second time

--- file1.py
+++ file1.py
@@ -6,3 +6,10 @@ total 12
 -rw-rw-r--. 1 sun sun  35 Aug 27 09:32 add.c
 -rw-rw-r--. 1 sun sun 143 Aug 27 14:07 file1.py
 -rw-rw-r--. 1 sun sun  29 Aug 27 10:00 ver.c
+total 28
+drwxrwxr-x. 3 sun sun    60 Aug 27 14:07 .
+drwx------. 3 sun sun   150 Aug 27 14:06 ..
+drwxrwxr-x. 8 sun sun 16384 Aug 27 14:08 .git
+-rw-rw-r--. 1 sun sun    35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun   290 Aug 27 14:07 file1.py
+-rw-rw-r--. 1 sun sun    29 Aug 27 10:00 ver.c
