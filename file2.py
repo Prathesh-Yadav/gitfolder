47300af778b882c37ae20275c63a9e68377bcb8b
Prathesh-Yadav <pratheshyadav67@gmail.com>
2024-08-27 14:10:38 +0000

    added file2.py first time

---
+++ file2.py
@@ -0,0 +1,15 @@
+total 24
+drwxrwxr-x. 3 sun sun    60 Aug 27 14:09 .
+drwx------. 3 sun sun   150 Aug 27 14:09 ..
+drwxrwxr-x. 8 sun sun 16384 Aug 27 14:09 .git
+-rw-rw-r--. 1 sun sun    35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun     0 Aug 27 14:09 file2.py
+-rw-rw-r--. 1 sun sun    29 Aug 27 10:00 ver.c
+total 12
+-rw-rw-r--. 1 sun sun  35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun 286 Aug 27 14:09 file2.py
+-rw-rw-r--. 1 sun sun  29 Aug 27 10:00 ver.c
+total 12
+-rw-rw-r--. 1 sun sun  35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun  29 Aug 27 10:00 ver.c
+-rw-rw-r--. 1 sun sun 433 Aug 27 14:10 file2.py
