239efb62d0b31f70af0246bb61a3b45e775095a5
Prathesh-Yadav <pratheshyadav67@gmail.com>
2024-08-27 14:08:13 +0000

    file1.py added newly

---
+++ file1.py
@@ -0,0 +1,8 @@
+total 8
+-rw-rw-r--. 1 sun sun 35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun  0 Aug 27 14:07 file1.py
+-rw-rw-r--. 1 sun sun 29 Aug 27 10:00 ver.c
+total 12
+-rw-rw-r--. 1 sun sun  35 Aug 27 09:32 add.c
+-rw-rw-r--. 1 sun sun 143 Aug 27 14:07 file1.py
+-rw-rw-r--. 1 sun sun  29 Aug 27 10:00 ver.c
